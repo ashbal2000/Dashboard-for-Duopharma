f6c4ef4aea1ddc13d3cca7747ef5ac6ea333f28b
Iqbal Abu Talib <ashbal08.iqbal@gmail.com>
2025-01-03 10:23:42 +0800

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -29,7 +29,7 @@ dash.register_page(__name__, path='/')
 
 
 # IN-USE
-query_conn = 'postgresql://trial_user:TCeWMoXo1PoduKMsP5fb1P9Woo6Gx1Bb@dpg-ctqvvs52ng1s73eqfvng-a.singapore-postgres.render.com/public_xp5i'
+query_conn = 'postgresql://dbb_dashboard_database_user:pOXlMkfXHlr9Ru7D4lM1QYIVHqfqmIsK@http://dpg-ctrkbm23esus73de2o20-a.singapore-postgres.render.com/dbb_dashboard_database'
 
 
 # --------------------------------------------- Definitions: Use for table ---------------------------------------------
